4899168d4b162a9fb1e9ef8222b174da14da2ad4
Stenly Adam <stenly.adam@unklab.ac.id>
2025-11-04 14:20:38 +0800

    click event & state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 //Komponen adalah fungsi yang return JSX
+import { useState } from "react";
 import HelloWorld from "./HelloWorld";
 
 const App = () => {
-  let hello = "Hello World";
+  // let hello = "Hello World"; //Deklarasi variabel biasa
+  const [hello, setHello] = useState("Hello World !!!"); //Deklarasi variable state
+
+  const handleClick = () => {
+    console.log("Button di klik");
+    // hello = "Selamat Datang"; //Cara merubah variabel biasa
+    setHello("Selamat Datang"); //Cara merubah variabel state
+
+    // console.log(hello);
+  };
+
   return (
     <>
       <div className="text-[25px] bg-blue-500 m-10 p-10 font-serif">
         {hello}
       </div>
-      <HelloWorld />
+      <HelloWorld text="Ini adalah Hello World 1" handleClick={handleClick} />
+      <HelloWorld text="Ini adalah Hello World 2" />
+      <button
+        className="bg-blue-500 hover:bg-blue-700 py-4 px-4 rounded-2xl text-white"
+        onClick={handleClick}
+      >
+        Click Me
+      </button>
     </>
   );
 };
--- src/HelloWorld/index.jsx
+++ src/HelloWorld/index.jsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
 import HelloMoon from "../HelloMoon";
 
-const HelloWorld = () => {
+//tipe data props adalah object
+const HelloWorld = ({ text, handleClick }) => {
+  const [hello, setHello] = useState("Welcome");
+  const handleChildClick = () => {
+    setHello("Selamat Datang");
+  };
   return (
     <div className="text-[25px] bg-red-500 m-10 p-10 font-serif">
-      Ini adalah komponen Hello World
+      {text}
+      {hello}
       <HelloMoon />
+      <button
+        className="bg-blue-500 hover:bg-blue-700 py-4 px-4 rounded-2xl text-white"
+        onClick={handleClick}
+      >
+        Click Me
+      </button>
+      <button
+        className="bg-green-500 hover:bg-green-700 py-4 px-4 rounded-2xl text-white"
+        onClick={handleChildClick}
+      >
+        Click Me
+      </button>
     </div>
   );
 };
